3c7c39871f6a5704cb02debf7e17ab5cb4396e18
vrushalibavare <vrushali.bavare@gmail.com>
2025-07-25 14:18:29 +0800

    changed domain names to include environment

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -5,7 +5,7 @@ module "shopmate" {
   aws_region         = "ap-southeast-1"
   app_count          = 1
   session_secret     = "dev-session-secret-change-me"
-  domain_name        = "shopmate.sctp-sandbox.com"
+  domain_name        = "shopmate.dev.sctp-sandbox.com"
   route53_zone_name  = "sctp-sandbox.com"
   create_route53_zone = false  # Using existing zone
 }
--- terraform/environments/uat/main.tf
+++ terraform/environments/uat/main.tf
@@ -1,8 +1,11 @@
 module "shopmate" {
   source = "../../"
   
-  environment    = "uat"
-  aws_region     = "ap-southeast-1"
-  app_count      = 2
-  session_secret = "uat-session-secret-change-me"
+  environment        = "uat"
+  aws_region         = "ap-southeast-1"
+  app_count          = 2
+  session_secret     = "uat-session-secret-change-me"
+  domain_name        = "shopmate.uat.sctp-sandbox.com"
+  route53_zone_name  = "sctp-sandbox.com"
+  create_route53_zone = false  # Using existing zone
 }
